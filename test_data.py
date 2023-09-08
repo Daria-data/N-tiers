2655b77d5b5d49312406eca75f82da808116df5f
Daria Antipova <79204168+Daria-data@users.noreply.github.com>
2023-09-08 10:19:22 +0200

    Delete test_data.py

--- test_data.py
+++
@@ -1,7 +0,0 @@
-from data import get_inventory
-
-def test_get_inventory():
-    inventory = get_inventory()
-    assert isinstance(inventory, dict)
-    assert "apple" in inventory
-    assert "banana" in inventory
